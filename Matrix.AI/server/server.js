1e3cac1e09a37c1872cd5484007fe9861ec99d2b
hars123 <harshitshukla4513@gmail.com>
2025-08-26 22:55:56 +0530

    add

--- Matrix.AI/server/server.js
+++ Matrix.AI/server/server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 const app = express()
 
 // ✅ call cors() instead of just passing cors
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
-  res.send('Server is Live!')
+  res.send('Server is Live!')      //home route
 })
+app.use(requireAuth())
 
 const PORT = process.env.PORT || 3000
 
